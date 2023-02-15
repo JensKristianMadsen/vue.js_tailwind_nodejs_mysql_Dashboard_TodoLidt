930d9b6f64e9e4db4f4571718030d68402ed03a0
Jens Kristian <jenskristian@Sebastians-MacBook-Pro-2.local>
2023-02-15 14:30:02 +0100

    Delete done refser

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -1,10 +1,11 @@
 <template>
     <!-- :class= "[objdet.value ?(true) 'reminder' :(else) '', 'task'(also want the task classe )  ]"-->
-    <div :class="[task.reminder ? 'reminder' : '', 'task']">
-        <h3>{{ task.text }}
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>
+            {{ task.text }}
              <!--Tailwind-->
           
-                <svg @click="onDelete(task.id)"  class="w-6 h-6"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                <svg @click="$emit('delete-task', task.id)" class="w-6 h-6"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             
@@ -20,11 +21,6 @@ export default {
     name: 'Task',
     props: {
         task: Object
-    },
-    methods: {
-        onDelete(id) {
-            this.$emit('delete-task', id)
-        },
     },
 }
 
