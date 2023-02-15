3d939a80c3f4583ab659d5d1981fdf2f25335a37
Jens Kristian <jenskristian@Sebastians-MacBook-Pro-2.local>
2023-02-15 12:02:37 +0100

    Create Delete keeep work

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -3,9 +3,11 @@
     <div :class="[task.reminder ? 'reminder' : '', 'task']">
         <h3>{{ task.text }}
              <!--Tailwind-->
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
+          
+                <svg @click="onDelete(task.id)"  class="w-6 h-6"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+            
             <!--Tailwind-->
         </h3>
         <p>{{ task.day }}</p>
@@ -18,6 +20,12 @@ export default {
     name: 'Task',
     props: {
         task: Object
+    },
+    methods: {
+        onDelete(id) {
+            this.$emit('delete-task', id)
+            console.log(id)
+        },
     },
 }
 
